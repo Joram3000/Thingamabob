411bb9c440d609b5d378369513b86744ba0be269
Joram3000 <98752947+Joram3000@users.noreply.github.com>
2022-05-03 13:02:30 +0200

    deployed version

---
+++ src/store/user/reducer.js
@@ -0,0 +1,29 @@
+import { LOG_OUT, LOGIN_SUCCESS, TOKEN_STILL_VALID } from "./actions";
+
+const initialState = {
+  token: localStorage.getItem("token"),
+  name: null,
+  email: null,
+  schoolname: null,
+  classname: null,
+  isteacher: null,
+  students: [],
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case LOGIN_SUCCESS:
+      localStorage.setItem("token", action.payload.token);
+      return { ...state, ...action.payload };
+
+    case LOG_OUT:
+      localStorage.removeItem("token");
+      return { ...initialState, token: null };
+
+    case TOKEN_STILL_VALID:
+      return { ...state, ...action.payload };
+
+    default:
+      return state;
+  }
+};
---
+++ src/store/user/selectors.js
@@ -0,0 +1,5 @@
+export const selectToken = (state) => state.user.token;
+
+export const selectUser = (state) => state.user;
+
+export const selectUserStudents = (state) => state.user.students;
